ff00447336a566103b5965c765445cb81fce6d22
Noah Rosamilia <nrosamil@grantstreet.com>
2024-06-27 14:32:54 -0400

    Add portal shot fade effect

--- Source/shot.lua
+++ Source/shot.lua
@@ -10,20 +10,37 @@ function Shot:init(from, to)
     local center = from + Vector.new(self.dir.x, self.dir.y)/2
     self:moveTo(center)
 
-    self:setSize(math.abs(self.dir.x) + 2, math.abs(self.dir.y) + 2)
+    self:setSize(math.abs(self.dir.x) + 4, math.abs(self.dir.y) + 4)
+
+    self:setZIndex(-2)
+
+    self.age = 0
+    self.length = math.sqrt(self.dir.x * self.dir.x + self.dir.y * self.dir.y)
 end
 
 function Shot:draw()
-    gfx.setLineWidth(2)
+    gfx.setLineWidth(4)
     gfx.setColor(gfx.kColorBlack)
 
     if self.dir.x > 0 and self.dir.y > 0 or self.dir.x < 0 and self.dir.y < 0 then
-        gfx.drawLine(1, 1, self.width - 1, self.height - 1)
+        gfx.drawLine(2, 2, self.width - 2, self.height - 2)
     else
-        gfx.drawLine(self.width - 1, 1, 1, self.height - 1)
+        gfx.drawLine(self.width - 2, 2, 2, self.height - 2)
+    end
+
+    gfx.setColor(gfx.kColorWhite)
+    for _=1, self.age*self.length do
+        local t = math.random()
+        local x = math.abs(self.dir.x)*(self.dir.x > 0 and t or 1 - t) + math.random(0, 4)
+        local y = math.abs(self.dir.y)*(self.dir.y > 0 and t or 1 - t) + math.random(0, 4)
+        gfx.drawPixel(x, y)
     end
 end
 
 function Shot:update()
-    self:remove()
+    self.age += 1
+    if self.age > 3 then
+        self:remove()
+    end
+    self:markDirty()
 end
