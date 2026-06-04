d6f12a8b1c2de8c84c97dcb58566cfdf47ceb294
Noah Rosamilia <noah@ivoah.net>
2026-06-04 13:46:46 -0400

    Switch to mill build tool

--- PortalEditor/src/main.scala
+++ PortalEditor/src/main.scala
@@ -4,8 +4,8 @@ import java.io.File
 import java.nio.file.Paths
 
 @main
-def main(projectRoot: String): Unit = {
-  val editor = Editor(Paths.get(projectRoot))
+def main: Unit = {
+  val editor = Editor(Paths.get("../Source"))
   editor.open()
 
   editor.loadLevel(File("../Source/levels/level7.json").getAbsoluteFile)
